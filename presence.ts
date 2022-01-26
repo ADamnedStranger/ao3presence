a10247d02c64d58cfaaae44ce15cf76340ad6f29
A Stranger <98432559+ADamnedStranger@users.noreply.github.com>
2022-01-26 14:41:43 +0100

    Updated presence.ts
    Redone the script, adding more information, like browsing fandoms, at home etc. when on specific sub-pages.

--- presence.ts
+++ presence.ts
@@ -1,50 +1,67 @@
 const presence = new Presence({
   //The client ID of the Application created at https://discordapp.com/developers/applications
-  clientId: "935651475378548736"
-  }),
+  clientId: "935651475378548736",
+});
 
-  strings = presence.getStrings({
-    play: "presence.playback.playing",
-    pause: "presence.playback.paused"
-  });
-
- const browsingTimestamp = Math.floor(Date.now() / 1000);
- let user: HTMLElement;
-
+const browsingTimestamp = Math.floor(Date.now() / 1000);
+let user: HTMLElement;
 
 presence.on("UpdateData", async () => {
   const presenceData: PresenceData = {
-	  
-    
     largeImageKey: "ArchiveOfOurOwn",
-    
     smallImageKey: "https://archiveofourown.org/images/ao3_logos/logo_42.png",
-    
     smallImageText: "Archive Of Our Own",
-    
-  }
-	 
-	 if (document.location.hostname === "www.archiveofourown.org") {
-		if (document.location.pathname === "/") {
-			presenceData.startTimestamp = browsingTimestamp;
-			presenceData.details = "Browsing Masterpieces";
-		} 
-		else if (document.location.pathname.includes("/works/")) {
-			presenceData.startTimestamp = browsingTimestamp;
-			presenceData.details = "Reads Masterpiece:";
-			presenceData.state = user.textContent;
-			presenceData.smallImageKey = "reading";
-		}
-	 };
-	 
-   
+  };
+  
+  const { pathname, hostname } = document.location;
 
-  //Update the presence with all the values from the presenceData object
- if (!presenceData.details) {
-		presenceData.startTimestamp = browsingTimestamp;
-		presenceData.details = "Browsing Masterpieces";
-		presenceData.state = document.querySelector("head > title").textContent;
-		presence.setActivity(presenceData);
-	} 
-	else presence.setActivity(presenceData);
+
+if (
+		hostname === "archiveofourown.org" ||
+		hostname === "www.archiveofourown.org"
+	) {
+		if (pathname === "/") {
+			presenceData.details = "At Home";
+			presenceData.startTimestamp = browsingTimestamp;
+		}
+		else if (pathname === "/media")
+			presenceData.details = "Browsing Fandoms";
+		else if (pathname.startsWith("/media/")) {
+			presenceData.details = "Browsing Fandoms";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+		} else if (pathname.includes("/chapters/")) {
+			presenceData.details = "Reading Chapter";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+		} else if (pathname === "/works") presenceData.details = "Browsing Masterpieces";
+		else if (pathname.startsWith("/works/")) {
+			presenceData.details = "Browsing Masterpieces";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+		} else if (pathname === "/users") presenceData.details = "Vieweing Profile";
+		else if (pathname.startsWith("/users/")) {
+			presenceData.details = "Viewing Profile";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+		} else if (pathname === "/comments") presenceData.details = "Checking comments";
+		else if (pathname.startsWith("/comments/")) {
+			presenceData.details = "Checking Comments";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+		} else if (pathname === "/edit") presenceData.details = "Editing their work";
+		else if (pathname.startsWith("/edit/")) {
+			presenceData.details = "Editing their work";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+		} else if (pathname === "/tags") presenceData.details = "Looking for something";
+		else if (pathname.startsWith("/tags/")) {
+			presenceData.details = "Looking for something";
+			presenceData.state = document.querySelector("head > title").textContent;
+			presenceData.startTimestamp = browsingTimestamp;
+	}};
+	
+  
+  	if (presenceData.details) presence.setActivity(presenceData);
+	else presence.setActivity();
 });
